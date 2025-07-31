439889054eef8db79ffb82d01fae733e71161c8c
LilyLilyChiu <Liling01327@gmail.com>
2025-07-31 12:23:53 +0800

    內部轉帳功能 7/31 1222

--- Bank/src/main/java/com/bank/account/controller/TransactionsController.java
+++ Bank/src/main/java/com/bank/account/controller/TransactionsController.java
@@ -6,20 +6,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.account.bean.Transactions;
-import com.bank.account.service.TransactionsService;
+import com.bank.account.service.transactions.DepositWithdrawalTxService;
+import com.bank.account.service.transactions.InternalTransferService;
 
 @RestController
 public class TransactionsController {
 	
 	@Autowired
-	private TransactionsService txService;
+	private DepositWithdrawalTxService dwTxSerice;
+	
+	@Autowired
+	private InternalTransferService interTxService;
 	
 	// 提款or存款
 	// Postman : {"accountId":"7100000237","transactionType":"存款","amount":"1000","memo":"07/31"}
-	@PutMapping("/account/transaction.controller")
-	public Transactions processTransactionsAction(@RequestBody Transactions txRequest) {
+	@PutMapping("/account/transaction/depositwithdrawal.controller")
+	public Transactions processDepositWithdrawalAction(@RequestBody Transactions txRequest) {
 		
-		return txService.insertTransactionAction(txRequest);
+		return dwTxSerice.depositWithdrawalAction(txRequest);
+	}
+	
+	@PutMapping("/account/transaction/internaltransfer.controller")
+	// 內部轉帳
+	// Postman :{"accountId":"7100000028","transactionType":"內部轉帳","toAccountId":"7100000237","amount":"1000","operatorId":1}
+	public Transactions processInternalTransferAction(@RequestBody Transactions txRequest) {
+		
+		return interTxService.internalTransferAction(txRequest);
 	}
 	
 }
--- Bank/src/main/java/com/bank/account/service/transactions/TransactionsService.java
+++ Bank/src/main/java/com/bank/account/service/transactions/TransactionsService.java
@@ -1,4 +1,4 @@
-package com.bank.account.service;
+package com.bank.account.service.transactions;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.bank.account.bean.Account;
 import com.bank.account.bean.Transactions;
 import com.bank.account.dao.TransactionsRepository;
+import com.bank.account.service.SerialControlService;
 import com.bank.utils.AccountUtils;
 
 import jakarta.transaction.Transactional;
@@ -19,9 +20,6 @@ public class TransactionsService {
 
 	@Autowired
 	private TransactionsRepository txRepos;
-	
-	@Autowired
-	private AccountServcie accountServcie;
 	
 	@Autowired
 	private SerialControlService scService;
@@ -59,82 +57,9 @@ public class TransactionsService {
 	}
 	
 	
-	// 帳戶提款、存款
-	public Transactions insertTransactionAction(Transactions txRequest) {
-		
-		String accountId = txRequest.getAccountId();
-		BigDecimal amount = txRequest.getAmount();
-		String transactionType = txRequest.getTransactionType();
-		String memo = txRequest.getMemo();
-		Integer operatorId = txRequest.getOperatorId();
-		
-		
-		String txStatus = "交易失敗"; // 預設交易失敗
-		
-		// 查詢帳戶	
-		Account accountRS = accountServcie.getByAccountId(accountId);
-		if( accountRS == null ) {
-			memo = "帳戶不存在" ;
-			return saveTransactionsRecord(null, transactionType, null, null, null 
-					, memo, txStatus, operatorId);
-		} 
-		
-		BigDecimal balance = accountRS.getBalance();
-		BigDecimal newBalance = null;
-		
-		// 檢查交易金額
-		if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0 ) {
-			memo = "交易金額輸入錯誤" ;
-			return saveTransactionsRecord(accountRS, transactionType, null, null, balance
-					, memo, txStatus, operatorId);
-		}
-		
-		switch (transactionType) {
-		
-			case "提款" :
-				
-				if( balance.compareTo(amount) >=0 ) {
-					// .subtract() 減法
-					newBalance = balance.subtract(amount);
-					txStatus = "交易成功";
-					
-				}else {
-					memo = "餘額不足" ;
-					return saveTransactionsRecord(accountRS, transactionType, null, null, balance
-							, memo, txStatus, operatorId);
-				}
-				break;
-				
-			case "存款" :
-				// .add() 加法 
-				newBalance = balance.add(amount);
-				txStatus = "交易成功";
-				break;
-			default : 
-				
-				memo = "交易類型錯誤" ;
-				return saveTransactionsRecord(accountRS, transactionType, null, null, balance
-						, memo, txStatus, operatorId);
-		}
-		
-		// 更新帳號餘額
-		if("交易成功".equals(txStatus)) {
-			
-			int updateRS = accountServcie.updateAccountBalance(accountId, newBalance);
-			
-			if( updateRS != 1 ) {
-				txStatus = "交易失敗";
-				memo = "更新帳戶餘額失敗" ; 
-				return saveTransactionsRecord(accountRS, transactionType, null, null, balance
-						, memo, txStatus, operatorId);
-			}
-		}
-		
-		
-		// 建立交易紀錄
-		return saveTransactionsRecord(accountRS, transactionType, null, null, newBalance != null ? newBalance : balance 
-						, memo, txStatus, operatorId);
-	}
+	
+	
+	// 轉帳交易 (內部帳戶)
 	
 	
 }
