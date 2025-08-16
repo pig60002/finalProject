f71c1f17c58ea980ebdd2b1c72a3bf88d918e02e
kevin <k0978237933@gmail.com>
2025-08-16 09:09:12 +0800

    no message

--- Bank/src/main/java/com/bank/member/controller/MemberController.java
+++ Bank/src/main/java/com/bank/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
