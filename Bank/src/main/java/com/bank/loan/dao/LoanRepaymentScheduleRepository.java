4b213dc2aec5d03e94138e1b9ae786394efee14c
mike <dy9638522@gmail.com>
2025-08-20 19:53:46 +0800

    no message

--- Bank/src/main/java/com/bank/loan/dao/LoanRepaymentScheduleRepository.java
+++ Bank/src/main/java/com/bank/loan/dao/LoanRepaymentScheduleRepository.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface LoanRepaymentScheduleRepository extends JpaRepository<LoanRepaymentSchedule, Long> {
     List<LoanRepaymentSchedule> findByLoanId(String loanId);
+    
+ // 新增：查詢指定貸款且狀態為 pending 的排程
+    List<LoanRepaymentSchedule> findByLoanIdAndPaymentStatus(String loanId, String paymentStatus);
+
 }
