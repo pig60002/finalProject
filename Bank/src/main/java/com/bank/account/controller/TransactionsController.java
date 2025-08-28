25ef842253194246f0883a1db9032822cef27cc6
LilyLilyChiu <Liling01327@gmail.com>
2025-08-28 15:57:38 +0800

    8/28 1557

--- Bank/src/main/java/com/bank/account/controller/TransactionsController.java
+++ Bank/src/main/java/com/bank/account/controller/TransactionsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,8 @@ import com.bank.account.bean.Transactions;
 import com.bank.account.service.transactions.DepositWithdrawalTxService;
 import com.bank.account.service.transactions.InternalTransferService;
 import com.bank.account.service.transactions.TransactionsService;
+import com.bank.member.bean.Worker;
+import com.bank.member.service.WorkerLogService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -33,22 +36,39 @@ public class TransactionsController {
 	@Autowired
 	private ObjectMapper mapper; // 由 Spring 注入，已註冊 JavaTimeModule
 
+	@Autowired
+	private WorkerLogService workerLogService;
+	
 	// 提款or存款
 	// Postman : {"accountId":"7100000237","transactionType":"存款","amount":"1000","memo":"07/31"}
 	@PutMapping("/account/transaction/depositwithdrawal")
 	public Transactions processDepositWithdrawalAction(@RequestBody Transactions txRequest) {
+		Object principal =SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		
-		return dwTxSerice.depositWithdrawalAction(txRequest);
+		Transactions txRs = dwTxSerice.depositWithdrawalAction(txRequest);
+		
+		if(principal instanceof Worker) {
+			Worker worker  = (Worker) principal;
+			workerLogService.logAction(worker.getwId(),txRequest.getTransactionType(),"帳戶編號:"+txRequest.getAccountId()+",金額:"+txRequest.getAmount()+",餘額:"+txRs.getBalanceAfter());
+		}
+		
+		return txRs;
 	}
 	
 	// 內部轉帳
 	// Postman :{"accountId":"7100000028","transactionType":"內部轉帳","toAccountId":"7100000237","amount":"1000","operatorId":1}
 	@PutMapping("/account/transaction/internaltransfer")
 	public Transactions processInternalTransferAction(@RequestBody Transactions txRequest) {
-		
+		Object principal =SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		try {
+			Transactions txRs = interTxService.internalTransferAction(txRequest);
 			
-			return interTxService.internalTransferAction(txRequest);
+			if(principal instanceof Worker) {
+				Worker worker  = (Worker) principal;
+				workerLogService.logAction(worker.getwId(),"轉帳","帳戶編號:"+txRequest.getAccountId()+",金額:"+txRequest.getAmount()+",餘額:"+txRs.getBalanceAfter());
+			}
+			
+			return txRs;
 			
 		} catch (RuntimeException e) {
 
